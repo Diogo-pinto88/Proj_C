a67f4feedce23302f663ec6bde4ebd1a86efa5af
Diogo Pinto <151055798+Diogo-pinto88@users.noreply.github.com>
2024-10-13 18:30:16 +0100

    Add files via upload

---
+++ modes.h
@@ -0,0 +1,13 @@
+#ifndef MODES_H
+#define MODES_H
+
+#include "functions.h"
+
+void Modo_P(char* s, park_list* l);
+void Modo_E(char* s, park_list* l);
+void Modo_S(char* s, park_list* l);
+void Modo_V(char* s, park_list* l);
+void Modo_F(char* s, park_list* l);
+park_list* Modo_R(char* s, park_list* l);
+
+#endif // MODES_H
